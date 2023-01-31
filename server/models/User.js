d426092831572ac8695f32dcd52158abbc33cefd
Leon Forsythe <lvforsythe95@gmail.com>
2023-01-31 13:58:29 -0500

    finished user model

--- server/models/User.js
+++ server/models/User.js
@@ -1,9 +1,45 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
+const Order = require("./Order");
 
 const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 5
+  },
+  CoOp: {
+    type: Schema.Types.ObjectId,
+    ref: "CoOp",
+  },
+  orders: [Order.schema],
+  Badges: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Badge",
+    },
+  ],
+});
 
-})
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
 
-const User = model('User', userSchema)
+  next();
+});
 
-module.exports = User
+userSchema.methods.isCorrectPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+const User = model("User", userSchema);
+
+module.exports = User;
