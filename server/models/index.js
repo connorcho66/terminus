976e764214f7f36ba387e95ce2d80f4ed045636f
Chelsea Runacres <chelsea.runacres@gmail.com>
2023-01-31 14:05:31 -0500

    added all models to index for export

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,8 @@
 const User = require("./User");
 const Product = require("./Product");
+const Badge = require('./Badge');
+const Category = require('./Category');
+const Order = require('./Order');
+const CoOp = require('./CoOp');
 
-module.exports = { User, Product };
+module.exports = { User, Product, CoOp, Category, Badge, Order };
