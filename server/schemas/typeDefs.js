d029c9baae4b8465c63964ec37925c2a6318e6b5
Chelsea Runacres <chelsea.runacres@gmail.com>
2023-01-31 14:35:44 -0500

    typeDefs worked collaboratively during class (:

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,75 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String!
+        password: String!
+        coOp: CoOp
+        orders: [Order]
+        badges: [Badge]
+    }
 
+    type Product {
+        _id: ID
+        name: String!
+        description: String
+        stock: Int
+        price: Float
+        image: String
+        category: Category
+    }
+
+    type CoOp {
+        _id: ID
+        name: String!
+        users: [User]
+        inventory: [Product]
+    }
+
+    type Order{
+    _id: ID
+    products: [Product]
+    }
+
+    type Badge {
+        _id: ID
+        name: String!
+        description: String
+        image: String
+    }
+
+    type Category {
+        _id: ID
+        name: String!
+    }
+
+    type Auth {
+        token: ID
+        user: User
+    }
+
+    type Checkout {
+        session: ID
+    }
+
+    type Query {
+        products(category: ID, name: String): [Product]
+        allProducts: [Product]
+        product(_id: ID!): Product
+        user(username: String): User
+        me: User
+        checkout(products: [ID]!): Checkout
+        order(_id: ID!): Order
+    }
+
+    type Mutation {
+        addUser(username: String!, email: String!, password: String!): Auth
+        addOrder(products: [ID]!): Order
+        updateUser(username: String, email: String, password: String): User
+        updateProduct(_id: ID!, quantity: Int!): Product
+        login(email: String!, password: String!): Auth
+    }
 `
 
 module.exports = typeDefs
