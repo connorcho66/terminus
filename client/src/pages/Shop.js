ccc36483acc0c6bcd54fbdf9cb827193c2adf280
Leon Forsythe <lvforsythe95@gmail.com>
2023-02-01 22:48:26 -0500

    added navbar to shop page

--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -1,26 +1,27 @@
 import { useQuery } from "@apollo/client";
 import React from "react";
 import { QUERY_ALL_PRODUCTS } from "../utils/queries";
-import ProductList from '../components/ProductList'
+import ProductList from "../components/ProductList";
 import { Box } from "@chakra-ui/react";
-
+import Nav from "../components/Nav";
 
 const Shop = () => {
   const { loading, data, error } = useQuery(QUERY_ALL_PRODUCTS);
 
   const products = data?.products || [];
 
-  if(error){
+  if (error) {
     console.log(error);
   }
   return (
-    <Box as="main" padding='1rem' backgroundColor='RGBA(0, 0, 0, 0.80)'>
+    <>
+      <Nav />
+      <Box as="main" padding="1rem" backgroundColor="RGBA(0, 0, 0, 0.80)">
         <h1>Shop:</h1>
-      {loading ? (<div>Loading...</div>) : (
-        <ProductList products={products} />
-      )}
-    </Box>
+        {loading ? <div>Loading...</div> : <ProductList products={products} />}
+      </Box>
+    </>
   );
 };
 
-export default Shop
+export default Shop;
